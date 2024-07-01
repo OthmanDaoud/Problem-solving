c46c22bfafb7d3d157f4870b8f21bea975af9a68
unknown <o.daoued@gmail.com>
2024-07-01 09:20:37 +0300

    semi final

--- app.js
+++ app.js
@@ -1,23 +1,20 @@
 // Q1 : Given an array arr[] of size N, you have to reverse the array using Stack
+function reverseArray(arr) {
+  let stack = [];
+  // Push all elements to the stack
+  for (let i = 0; i < arr.length; i++) {
+    stack.push(arr[i]);
+  }
+  // Pop all elements from the stack and put them back in the array
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = stack.pop();
+  }
+  return arr;
+}
 
-// class Stack {
-//   constructor() {
-//     this.array = [];
-//     this.reverse = [];
-//   }
-//   reversingArray(array) {
-//     for (let i = 0; i < array.length; i++) {
-//       this.array.push(array[i]);
-//     }
-//     for (let i = 0; i < array.length; i++) {
-//       this.reverse.push(pull(array[array.length - 1]));
-//     }
-//   }
-// }
-
-// let array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
-// console.log(reversingArray(array));
+// Example usage
+let arr = [1, 2, 3, 4, 5];
+console.log(reverseArray(arr)); // Output: [5, 4, 3, 2, 1]
 
 // Q2 : Implementing a Queue Data Structure using Two Stacks
 
@@ -35,6 +32,35 @@ let array2 = [1, 2, 3, 4, 5];
 console.log(sumRecursion(array2, array2.length));
 
 // Q4 : Create a function that takes a LinkedList and reverses it
+class ListNode {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function reverseLinkedList(head) {
+  let prev = null;
+  let current = head;
+  while (current !== null) {
+    let nextNode = current.next;
+    current.next = prev;
+    prev = current;
+    current = nextNode;
+  }
+  return prev;
+}
+
+// Example usage
+let head = new ListNode(1);
+head.next = new ListNode(2);
+head.next.next = new ListNode(3);
+head = reverseLinkedList(head);
+while (head !== null) {
+  console.log(head.value);
+  head = head.next;
+}
+// Output: 3 2 1
 
 // Q5 :  Create a function that takes a LinkedList and deletes the middle node in it and returns it
 class ListNode {
